048230bdafa9f853040e2054baab8aaabf0d9ee9
Rinaldis damien <damiendamien761@gmail.com>
2026-05-12 16:48:38 +0200

    Javadoc

--- out/src/calcultableau/CalculTab.java
+++ out/src/calcultableau/CalculTab.java
@@ -39,9 +39,6 @@ public class CalculTab {
      * @param note la note à ajouter (valeur entière, généralement entre 0 et 20)
      */
     public void ajouterNote(int note) {
-       if (note < 0 || note > 20) {
-          throw new IllegalArgumentException("La note doit être comprise entre 0 et 20, valeur reçue : " + note);
-       }
         notes.add(note);
     }
 
